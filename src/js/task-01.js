ded85b8fa95fd831ab32c3dd57a447213d3098af
AVKostin <90350582+AVKostin@users.noreply.github.com>
2022-01-16 23:04:42 +0200

    mistake data

--- src/js/task-01.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelectorAll("li.item");
-
-console.log(`Number of categories: ${list.length}`);
-const uEl = document.querySelectorAll("#categories > li");
-uEl.forEach((el) => {
-  console.log("\nCategory:", el.firstElementChild.textContent);
-  console.log("Elements:", el.lastElementChild.children.length);
-});
--- src/js/task-06.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.getElementById("validation-input");
-const inputLength = inputEl.dataset.length;
-
-inputEl.addEventListener("blur", (event) => {
-  event.target.value.length === Number(inputLength)
-    ? inputEl.setAttribute("class", "valid")
-    : inputEl.setAttribute("class", "invalid");
-});
