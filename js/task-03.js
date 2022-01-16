a2eca03f728db6bbe444cb108ee1f34b34c15dac
AVKostin <alexander.kostin.ua@gmail.com>
2022-01-16 23:56:44 +0200

    add style in Task#3

--- js/task-03.js
+++ js/task-03.js
@@ -3,9 +3,12 @@ import images from "./data_images.js";
 const ulEl = document.querySelector(".gallery");
 
 Object.assign(ulEl.style, {
+  width: "1000px",
+  margin: "0 auto",
   display: "flex",
-  flexWrap: "wrap",
+  flexWrap: "no-wrap",
   listStyle: "none",
+  justifyContent: "space-between",
 });
 
 const image = images.reduce(
