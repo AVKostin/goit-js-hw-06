b0e5d53d36893e6492102bcac2db3d16a4ad5d6c
AVKostin <alexander.kostin.ua@gmail.com>
2022-01-17 04:28:39 +0200

    Update task-03.js
    other solve

--- js/task-03.js
+++ js/task-03.js
@@ -2,6 +2,26 @@ import images from "./data_images.js";
 
 const ulGalleryEl = document.querySelector(".gallery");
 
+// const image = images.reduce(
+//   (acc, { url, alt }) =>
+//     (acc += `<li><img src='${url}', alt='${alt}' width = "300" height = "200"/></li>`),
+//   ""
+// );
+// ulGalleryEl.insertAdjacentHTML("beforeend", image);
+
+const galleryArr = images.map((image) => {
+  const listEl = document.createElement("li");
+  const imgEl = document.createElement("img");
+  imgEl.src = image.url;
+  imgEl.alt = image.alt;
+  imgEl.width = 300;
+  imgEl.height = 200;
+  listEl.appendChild(imgEl);
+  return listEl;
+});
+
+ulGalleryEl.append(...galleryArr);
+
 Object.assign(ulGalleryEl.style, {
   width: "1000px",
   margin: "0 auto",
@@ -10,10 +30,3 @@ Object.assign(ulGalleryEl.style, {
   listStyle: "none",
   justifyContent: "space-between",
 });
-
-const image = images.reduce(
-  (acc, { url, alt }) =>
-    (acc += `<li><img src='${url}', alt='${alt}' width = "300" height = "200"/></li>`),
-  ""
-);
-ulGalleryEl.insertAdjacentHTML("beforeend", image);
