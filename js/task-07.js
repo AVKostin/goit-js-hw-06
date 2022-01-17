14e10bbdc4771535e7fecdd2d226f3f26de0a300
AVKostin <alexander.kostin.ua@gmail.com>
2022-01-17 05:00:11 +0200

    Update task-07.js
    Easy Solvations!

--- js/task-07.js
+++ js/task-07.js
@@ -1,14 +1,7 @@
-const spanEl = document.getElementById("text");
-const inputEl = document.getElementById("font-size-control");
-const spanTextSize = () => {
+const inputEl = document.querySelector("input");
+const spanEl = document.querySelector("span");
+const sliderTextSize = () => {
   spanEl.style.fontSize = inputEl.value + "px";
 };
-spanTextSize();
-
-inputEl.addEventListener("input", () => {
-  sliderTextSize();
-});
-
-const sliderTextSize = () => {
-  spanTextSize();
-};
+sliderTextSize();
+inputEl.addEventListener("input", sliderTextSize);
