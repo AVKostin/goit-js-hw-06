04b02e2b0401046d2b68847faa254d9dc093e634
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-02 18:41:11 +0200

    refactoring

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,17 +1,17 @@
 const counterValue = document.querySelector("#value");
 let valueEl = Number(counterValue.textContent);
 const countText = () => {
-  counterValue.innerText = valueEl;
+    counterValue.innerText = valueEl;
 };
 
 const dekrBtn = document.querySelector("#counter").firstElementChild;
 const inkrBtn = document.querySelector("#counter").lastElementChild;
 
-inkrBtn.addEventListener("click", (event) => {
-  valueEl += 1;
-  countText();
+inkrBtn.addEventListener("click", () => {
+    valueEl += 1;
+    countText();
 });
-dekrBtn.addEventListener("click", (event) => {
-  valueEl -= 1;
-  countText();
+dekrBtn.addEventListener("click", () => {
+    valueEl -= 1;
+    countText();
 });
