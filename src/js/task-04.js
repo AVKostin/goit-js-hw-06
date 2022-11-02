77d07dcd5982284b54513381de0cd67c88582f70
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-02 18:42:31 +0200

    //Создем переменную в которой будет храниться текущее значение счетчика и инициализируем её значением 0. let counterValue = 0;
    // Находим елементы button и span
    const plusBtnEl = document.querySelector('button[data-action="increment"]');
    const minusBtnEl = document.querySelector('button[data-action="decrement"]');
    const counterEl = document.querySelector('#value');
    
    // Добавляем слушателя события (клик на кнопке), 
    // внутри кот.увеличиваем и уменьшаем текущее значение counterValue, 
    // меняем текстовое содержание counterEl = counterValue
    plusBtnEl.addEventListener('click', () => {
        counterValue += 1;
        counterEl.textContent = counterValue;
    });
    
    minusBtnEl.addEventListener('click', () => {
        counterValue -= 1;
        counterEl.textContent = counterValue;
    });

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,17 +1,33 @@
-const counterValue = document.querySelector("#value");
-let valueEl = Number(counterValue.textContent);
-const countText = () => {
-    counterValue.innerText = valueEl;
-};
+// const counterValue = document.querySelector("#value");
+// let valueEl = Number(counterValue.textContent);
+// const countText = () => {
+//     counterValue.innerText = valueEl;
+// };
 
-const dekrBtn = document.querySelector("#counter").firstElementChild;
-const inkrBtn = document.querySelector("#counter").lastElementChild;
+// const dekrBtn = document.querySelector("#counter").firstElementChild;
+// const inkrBtn = document.querySelector("#counter").lastElementChild;
 
-inkrBtn.addEventListener("click", () => {
-    valueEl += 1;
-    countText();
+// inkrBtn.addEventListener("click", () => {
+//     valueEl += 1;
+//     countText();
+// });
+// dekrBtn.addEventListener("click", () => {
+//     valueEl -= 1;
+//     countText();
+// });
+
+let counterValue = 0;
+
+const plusBtnEl = document.querySelector('button[data-action="increment"]');
+const minusBtnEl = document.querySelector('button[data-action="decrement"]');
+const counterEl = document.querySelector("#value");
+
+plusBtnEl.addEventListener("click", () => {
+    counterValue += 1;
+    counterEl.textContent = counterValue;
 });
-dekrBtn.addEventListener("click", () => {
-    valueEl -= 1;
-    countText();
+
+minusBtnEl.addEventListener("click", () => {
+    counterValue -= 1;
+    counterEl.textContent = counterValue;
 });
