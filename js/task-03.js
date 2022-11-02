b19c6b9b660c8a1d55b60deef556d64ccb8f8eb6
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-02 19:45:08 +0200

    del coments

--- js/task-03.js
+++ js/task-03.js
@@ -3,30 +3,17 @@ import images from "./data_images.js";
 const ulGalleryEl = document.querySelector(".gallery");
 
 const image = images.reduce(
-  (acc, { url, alt }) =>
-    (acc += `<li><img src='${url}', alt='${alt}' width = "300" height = "200"/></li>`),
-  ""
+    (acc, { url, alt }) =>
+        (acc += `<li><img src='${url}', alt='${alt}' width = "300" height = "200"/></li>`),
+    "",
 );
 ulGalleryEl.insertAdjacentHTML("beforeend", image);
 
 Object.assign(ulGalleryEl.style, {
-  width: "1000px",
-  margin: "0 auto",
-  display: "flex",
-  flexWrap: "no-wrap",
-  listStyle: "none",
-  justifyContent: "space-between",
+    width: "1000px",
+    margin: "0 auto",
+    display: "flex",
+    flexWrap: "no-wrap",
+    listStyle: "none",
+    justifyContent: "space-between",
 });
-
-// const galleryArr = images.map((image) => {
-//   const listEl = document.createElement("li");
-//   const imgEl = document.createElement("img");
-//   imgEl.src = image.url;
-//   imgEl.alt = image.alt;
-//   imgEl.width = 300;
-//   imgEl.height = 200;
-//   listEl.appendChild(imgEl);
-//   return listEl;
-// });
-
-// ulGalleryEl.append(...galleryArr);
