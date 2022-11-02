f00676b86d4c47977eeda9681b57d397cbf65756
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-02 19:35:10 +0200

    change code

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,16 +1,19 @@
 import images from "./data_images.js";
 
-const ulEl = document.querySelector(".gallery");
-
-Object.assign(ulEl.style, {
-  display: "flex",
-  flexWrap: "wrap",
-  listStyle: "none",
-});
+const ulGalleryEl = document.querySelector(".gallery");
 
 const image = images.reduce(
-  (acc, { url, alt }) =>
-    (acc += `<li><img src='${url}', alt='${alt}' width = "300" height = "200"/></li>`),
-  ""
+    (acc, { url, alt }) =>
+        (acc += `<li><img src='${url}', alt='${alt}' width = "300" height = "200"/></li>`),
+    "",
 );
-ulEl.insertAdjacentHTML("beforeend", image);
+ulGalleryEl.insertAdjacentHTML("beforeend", image);
+
+Object.assign(ulGalleryEl.style, {
+    width: "1000px",
+    margin: "0 auto",
+    display: "flex",
+    flexWrap: "no-wrap",
+    listStyle: "none",
+    justifyContent: "space-between",
+});
